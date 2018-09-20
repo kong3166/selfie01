08f2cd18f0523c05d7d79f6df71f67330044f766
kong3166 <1024239374@qq.com>
2018-09-20 20:07:19 +0800

    把原图片缩小并且变成灰度图片显示

--- face.py
+++ face.py
@@ -1,14 +1,26 @@
+# selfie01
+
 import cv2
+import dlib
 
 video_capture = cv2.VideoCapture(0)
 
+def trace_human_face(image):
+    return image
+
 while True:
     ret,frame = video_capture.read()
-    cv2.imshow('Example',frame)
+    scale = 0.5
+    thumb = cv2.resize(frame, None, fx = scale, fy = scale,interpolation = cv2.INTER_AREA)
+    gray = cv2.cvtColor(thumb , cv2.COLOR_BGR2GRAY)
+
+    trace_huan_face(gray)
+    cv2.imshow('Example',gray)
 
     if cv2.waitKey(1)  & 0xff  == ord('q'):
         break
 
 video_capture.release()
 cv2.destroyAllWindows()
-# selfie01
+
+# hello-word
